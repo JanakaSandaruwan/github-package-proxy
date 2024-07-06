56d3fb88b1002d321765b197f34733a322390f56
xlight05 <anjanasupun05@gmail.com>
2024-07-06 12:31:58 +0530

    Modify port to 5000

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.get('*', async (req, res) => {
     }
 });
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
