c8c35622a994e8ecf4794a90cf2f80f6a0692f3a
xlight05 <anjanasupun05@gmail.com>
2024-07-07 10:53:52 +0530

    Port

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app.get('/org/ballerinalang/:package/:version/:file', async (req, res) => {
     }
 });
 
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
