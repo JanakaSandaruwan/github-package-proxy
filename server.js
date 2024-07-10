0bcccf876987cfb376f5dd2997cebb95bab9747b
Janaka Sandaruwan <35717600+JanakaSandaruwan@users.noreply.github.com>
2024-07-11 00:10:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.get('/org/ballerinalang/:package/:version/:file', async (req, res) => {
         res.status(error.response ? error.response.status : 500).send('Unknown error');
     }
 });
-
+console.log(process.env.PORT)
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
